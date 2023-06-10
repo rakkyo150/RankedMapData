10fdd24fce9b765ebcc7b10065bb678509abc49b
rakkyo150 <86054813+rakkyo150@users.noreply.github.com>
2023-06-11 02:28:54 +0900

    バグフィックス

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import DataGetter
 print("全更新")
 dataGetter = DataGetter.DataGetter()
 
-outcome_df = dataGetter.get_data()
+outcome_df = dataGetter.get_data(False)
 
 dataGetter.save(outcome_df, f'out/outcome.csv')
