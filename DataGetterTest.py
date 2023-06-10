940cb6d0ac1ced7f747d9bcb7291a682d3570815
rakkyo150 <86054813+rakkyo150@users.noreply.github.com>
2023-06-10 23:49:06 +0900

    取得回数の表示ミス修正

--- DataGetterTest.py
+++ DataGetterTest.py
@@ -12,11 +12,11 @@ class TestGetDataPerPage(unittest.TestCase):
         page_number = 1
         result = False
         if len(self.dataGetter.set_data().index) == 0:
-            result = self.dataGetter.get_data_per_page(previous_hash_list, get_data_from_beat_saver_count, page_number)
+            get_data_from_beat_saver_count, result = self.dataGetter.get_data_per_page(previous_hash_list, get_data_from_beat_saver_count, page_number)
 
         # Make assertions about the results
         self.assertTrue(result)
-        self.assertNotEqual(len(self.dataGetter.idList),0)
+        self.assertNotEqual(len(self.dataGetter.idList), 0)
 
     def test_get_data_per_page_failure(self):
 
@@ -24,28 +24,29 @@ class TestGetDataPerPage(unittest.TestCase):
         previous_hash_list = []
         get_data_from_beat_saver_count = 0
         page_number = 999999
-        dataGetter = DataGetter.DataGetter()
+        dataGetterFailure = DataGetter.DataGetter()
 
         # Call the function to be tested
-        result = dataGetter.get_data_per_page(previous_hash_list, get_data_from_beat_saver_count, page_number)
+        get_data_from_beat_saver_count, result = dataGetterFailure.get_data_per_page(previous_hash_list, get_data_from_beat_saver_count, page_number)
 
         # Make assertions about the results
         self.assertFalse(result)
-        self.assertEqual(len(dataGetter.idList),0)
+        self.assertEqual(get_data_from_beat_saver_count, 0)
+        self.assertEqual(len(dataGetterFailure.idList), 0)
         
     def test_set_data(self):
         # Set up initial values for test
-        previous_hash_list = []
-        get_data_from_beat_saver_count = 0
-        page_number = 1
         if len(self.dataGetter.set_data().index) == 0:
+            previous_hash_list = []
+            get_data_from_beat_saver_count = 0
+            page_number = 1
             _ = self.dataGetter.get_data_per_page(previous_hash_list, get_data_from_beat_saver_count, page_number)
 
         # Call the function to be tested
         outcome_df = self.dataGetter.set_data()
 
         # Make assertions about the results
-        self.assertIsNotNone(outcome_df.iloc[1,0])
+        self.assertIsNotNone(outcome_df.iloc[1, 0])
 
 if __name__ == '__main__':
     unittest.main()
